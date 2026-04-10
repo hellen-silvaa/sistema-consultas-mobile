09c02662df6d440586d11497ef3868191fd50baa
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-09 21:48:28 -0300

    feat: componentizacao react native aula 2603

---
+++ components/Index.ts
@@ -0,0 +1,5 @@
+export { default as ConsultaCard } from "./ConsultaCard";
+
+export { default as Loading } from "./Loading";
+export { default as EmptyState } from "./EmptyState";
+export { default as Header } from "./Header";
